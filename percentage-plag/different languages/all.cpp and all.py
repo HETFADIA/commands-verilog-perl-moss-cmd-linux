c571664d2dee6bb4a2243fc7abc124a073c79817
Het <62541263+HETFADIA@users.noreply.github.com>
2020-10-26 01:42:18 +0530

    Create all.cpp and all.py

---
+++ percentage-plag/different languages/all.cpp and all.py
@@ -0,0 +1,27 @@
+http://moss.stanford.edu/results/9/4165583011667
+
+for j in range(10):
+    print(j)
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i=0;i<10;i+=1){
+        cout<<i<<endl;
+    }
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i=0;i<10;i=i+1){
+        cout<<i<<endl;
+    }
+}
+for i in range(10):
+    print(i)
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i=0;i<10;i++){
+        cout<<i<<endl;
+    }
+}
