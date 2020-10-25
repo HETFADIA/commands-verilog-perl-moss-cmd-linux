af856ca9511c57c8baf1e7466f8dde1ef5e07d53
Het <62541263+HETFADIA@users.noreply.github.com>
2020-10-26 01:37:06 +0530

    Create for.py forloop.py and for.cpp

---
+++ percentage-plag/different languages/for.py forloop.py and for.cpp
@@ -0,0 +1,14 @@
+http://moss.stanford.edu/results/5/5922551640282/
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i=0;i<10;i++){
+        cout<<i<<endl;
+    }
+}
+
+for i in range(10):
+    print(i)
+    
+for j in range(10):
+    print(j)
