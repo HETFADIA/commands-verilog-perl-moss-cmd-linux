7fccfc27b33c0193de1c2b51ca80d59de798d3aa
Het <62541263+HETFADIA@users.noreply.github.com>
2020-10-26 01:12:31 +0530

    Create fun non-function.cpp

---
+++ percentage-plag/cpp/fun non-function.cpp
@@ -0,0 +1,21 @@
+http://moss.stanford.edu/results/7/1098561939756
+File one:
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int &a){
+    a++;
+}
+int main() {
+    int a=0;
+    fun(a);
+}
+
+File two:
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a=0;
+    a++;
+}
