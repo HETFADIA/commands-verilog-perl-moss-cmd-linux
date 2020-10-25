6c3adb078193baf18bda4cc0e1d5d09d9eea5382
Het <62541263+HETFADIA@users.noreply.github.com>
2020-10-26 01:14:21 +0530

    Create var and morevar.cpp

---
+++ percentage-plag/cpp/var and morevar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a=0;
+}
+
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a=0;
+    int b;
+    int c;
+    int d;
+}
+
+http://moss.stanford.edu/results/7/6905090972754/
