97c2c70d70df08118fd8e9cfe6ccaa2ad0da2908
Het <62541263+HETFADIA@users.noreply.github.com>
2020-11-01 01:59:55 +0530

    Create function.py and useful.py

---
+++ percentage-plag/python/function.py and useful.py
@@ -0,0 +1,26 @@
+http://moss.stanford.edu/results/9/2256031721803
+for _ in range(int(input())):
+    n=int(input())
+    print(0,n)
+    
+    
+    
+
+from sys import stdin,stdout
+from math import gcd,log2,log10,floor,ceil,sqrt;
+import math;
+from collections import defaultdict,OrderedDict
+from bisect import bisect_left
+n=0
+def is_prime(n):
+    for i in range(2,int(sqrt(n)+1)):
+        if n%i==0:
+            return 0;
+    return 1;
+def fun():
+    if isprime(n):
+        return 1;
+    return 0;
+for _ in range(int(input())):
+    n=int(input())
+    print(0,n)
