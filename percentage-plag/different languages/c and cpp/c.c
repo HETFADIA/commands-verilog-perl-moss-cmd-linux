71b5b745385bfe841102acbae34ced00604bcac9
Het <62541263+HETFADIA@users.noreply.github.com>
2020-11-15 14:18:30 +0530

    Create c.c

---
+++ percentage-plag/different languages/c and cpp/c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+    for(int i=0;i<100;i++){
+        printf("a");
+    }
+    printf("b");
+    int i=0;
+    while(i<=5){
+        i++;
+    }
+    printf("Hello World\n");
+  return 0;
+}
